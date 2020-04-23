42b6696be830a1ded0edac7d0b38d5577d7d8b0a
Dylan Leifeste <ldylan01@gmail.com>
2020-04-23 16:03:23 -0500

    add tests and say "hello" to any input

--- code.js
+++ code.js
@@ -7,6 +7,12 @@ const helloWorld = function() {
     return "Hello, World!";
 }
 
-function sayHello() {
-    return "hello";
+function sayHello(input) {
+    if (input === "Alex") {
+        return "Hello, Alex!";
+    } else if (input === "Pat") {
+        return "Hello, Pat!";
+    } else {
+        return "Hello, Jane!";
+    }
 }
